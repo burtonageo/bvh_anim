ebd33deee88fe794e6509c7a4854a1c13adbe66e
George Burton <burtonageo@gmail.com>
2019-06-07 13:11:39 +0100

    Add test for nonstandard formatting

--- tests/parse.rs
+++ tests/parse.rs
@@ -42,3 +42,91 @@ fn string_parse_small() {
 
     assert_eq!(bvh, bvh_from_macro);
 }
+
+#[test]
+#[ignore] // @TODO(burtonageo): Turn this on when the nom parser lands
+fn nonstandard_formatting() {
+    const BVH_STRING: &[u8] = br#"
+    HIERARCHY ROOT Base {
+        OFFSET 0.0 0.0
+        0.0
+        CHANNELS 6
+        Xposition
+        Yposition
+        Zposition
+        Zrotation
+        Xrotation
+        Yrotation
+        Joint End {
+            Offset
+            0.0
+            15.0
+            0.0
+            CHANNELS 3 Zrotation
+            Xrotation Yrotation
+            End Site { Offset 0.0 0.0 30.0 }
+        }
+        Joint
+        End2
+            { Offset 15.0 0.0 0.0
+            Channels 3 Zrotation Xrotation
+            Yrotation
+            End
+            Site
+            {
+                Offset
+                30.0
+                0.0
+                0.0
+            }
+
+    }}
+
+    MOTION Frames : 2
+    Frame
+
+
+
+    Time
+    :
+    0.0333333333333333333
+
+    0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+    "#;
+    let bvh = bvh_anim::from_bytes(BVH_STRING).unwrap();
+
+    let bvh_from_macro = bvh_anim::bvh! {
+        HIERARCHY
+        ROOT Base
+        {
+            OFFSET 0.0 0.0 0.0
+            CHANNELS 6 Xposition Yposition Zposition Zrotation Xrotation Yrotation
+            JOINT End
+            {
+                OFFSET 0.0 0.0 15.0
+                CHANNELS 3 Zrotation Xrotation Yrotation
+                End Site
+                {
+                    OFFSET 0.0 0.0 30.0
+                }
+            }
+            JOINT End2
+            {
+                OFFSET 15.0 0.0 0.0
+                CHANNELS 3 Zrotation Xrotation Yrotation
+                End Site
+                {
+                    OFFSET 30.0 0.0 0.0
+                }
+            }
+        }
+
+        MOTION
+        Frames: 2
+        Frame Time: 0.033333333333
+        0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0
+        1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+    };
+
+    assert_eq!(bvh, bvh_from_macro);
+}
